7962076357a9fd79c343d297a46ec3e7f3eb1754
VidushiMangal <vidushi.mangal@gmail.com>
2025-11-25 11:52:43 +0530

    Add Sorting/Searching/API Serching methods

--- fastapi_app/app/database.py
+++ fastapi_app/app/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import OperationalError
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost/fastapi_db"
-print("Server Connected")
-
 engine = create_engine(DATABASE_URL)
-print("Session Started")
+try:
+    with engine.connect() as conn:
+        print(" Database (Postgres) Connected Successfully!")
+except OperationalError as e:
+    print("Database (Postgres) Connection Failed!")
+    print(e)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
+
+
--- fastapi_app/app/routers/items.py
+++ fastapi_app/app/routers/items.py
@@ -4,6 +4,7 @@ from app.database import SessionLocal
 from app.models.item_db import ItemDB # Database Model
 from app.models.item_model import Item # Pydantic Model
 
+
 router= APIRouter()
 
 def get_db():
@@ -26,14 +27,19 @@ def create_item(item: Item, db: Session = Depends(get_db)):
 
 #Listing All Records
 @router.get("/items")
-def get_items(db: Session = Depends(get_db)):
+def get_item_all(db: Session = Depends(get_db)):
     print("Listing All Records")
     return db.query(ItemDB).all()
 
 #Retrieving A Record with Given ID
 @router.get("/items/{item_id}")
-def get_item(item_id: int, db: Session = Depends(get_db)):
+def get_item_by_id(item_id: int, db: Session = Depends(get_db)):
     return db.query(ItemDB).filter(ItemDB.id == item_id).first()
+
+# Filtering on the basis of type(example name=Laptop)
+@router.get("/items/name/{item_name}")
+def get_item_by_name(item_name:str, db: Session = Depends(get_db)):
+    return db.query(ItemDB).filter(ItemDB.name == item_name).all()
 
 #Updating A Record with Given ID    
 @router.put("/items/{item_id}")
@@ -64,3 +70,26 @@ def delete_item(item_id: int, db: Session = Depends(get_db)):
 def get_items_paginated(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     items = db.query(ItemDB).offset(skip).limit(limit).all()
     return items
+
+#Filtering on the basis of price
+@router.get("/items_filter")
+def filter_items(min_price: float = 0,max_price: float = 1000000,db: Session = Depends(get_db)):
+    return db.query(ItemDB).filter(ItemDB.price >= min_price,ItemDB.price <= max_price).all()
+
+#Sorting by price/name where price in ascending is specified as default
+@router.get("/items_sort")
+def sort_items(order_by: str = "price", db: Session = Depends(get_db)): # price is specify as default parameter
+
+    if order_by == "price":
+        return db.query(ItemDB).order_by(ItemDB.price).all() # price ascending
+    elif order_by == "-price":
+        return db.query(ItemDB).order_by(ItemDB.price.desc()).all() # price descending
+    elif order_by == "name":
+        return db.query(ItemDB).order_by(ItemDB.name).all() # name ascending
+    else:
+        return db.query(ItemDB).all() # return as it is 
+
+# Item Search which has given character anywhere in string (API Search)
+@router.get("/items_search")
+def search_items(char: str, db: Session = Depends(get_db)):
+    return db.query(ItemDB).filter(ItemDB.name.ilike(f"%{char}%")).all()
