d1e22b42f5ffe9d6c9ec54c3e2e2ca112e09ac3e
vineetmangal <vineet.mangal@gmail.com>
2025-12-09 14:43:38 +0530

    addupdated  gitignore

--- fastapi_app/app/main.py
+++ fastapi_app/app/main.py
@@ -1,17 +1,27 @@
+import os
 from fastapi import FastAPI
 from app.routers import items,auth
 from app.database import engine
 from app.models import item_model
 from app.models import item_db, users_db
+from dotenv import load_dotenv
+
+load_dotenv()
 
 item_db.Base.metadata.create_all(bind=engine)
 users_db.Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
 
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+app = FastAPI(debug=DEBUG)
+
 app.include_router(items.router)
 app.include_router(auth.router)
 
+@app.get("/healthz")
+def health():
+    return {"status": "ok"}
 
 
 
