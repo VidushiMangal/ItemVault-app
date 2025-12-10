4fdafcb4fb5611e8da5089186c8a314d7bacd2b1
vineetmangal <vineet.mangal@gmail.com>
2025-12-10 11:07:45 +0530

    added loggin.py

---
+++ fastapi_app/app/utils/logging.py
@@ -0,0 +1,10 @@
+# app/utils/logging.py
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+)
+logger = logging.getLogger("fastapi_app")
